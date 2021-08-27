34a76dba44c668c9dd970d1262d52722651f6c71
nkal0516 <68945136+nkal0516@users.noreply.github.com>
2021-08-27 16:16:07 +0900

    Add files via upload

--- lotto/ballMaker.vue
+++ lotto/ballMaker.vue
@@ -1,17 +1,32 @@
 <template>
-  <div class="ball" :style="{ background }">{{number}}</div>
+  <div class="ball" :style="styleObject">{{number}}</div>
 </template>
 
 <script>
 export default {
     name: 'ballMaker',
-    props:['number'],
-    data() {
+    props:{
+        number : Number,
+    },
+    computed: {
+        styleObject(){
         let background;
+        if(this.number <= 10){
+            background = 'red';
+        }else if(this.number <= 20){
+            background = 'orange';
+        }else if(this.number <= 30){
+            background= 'yellow';
+        }else if(this.number <= 40){
+            background = 'green';
+        }else{  
+            background ='blue';
+        }
         return {
            background,
-        };
-    },
+            };
+        }
+        },
 
 }
 </script>
